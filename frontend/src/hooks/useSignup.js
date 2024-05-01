8ee4051b3835068ac702b42002698129ccd4c852
unknown <you@example.com>
2024-05-01 08:43:17 +0530

    new update

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -24,17 +24,20 @@ const useSignup = () => {
 
     setLoading(true);
     try {
-      const res = await fetch("https://chatbox-backend-1xq1.onrender.com/", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          fullName,
-          username,
-          password,
-          confirmPassword,
-          gender,
-        }),
-      });
+      const res = await fetch(
+        "https://chatbox-backend-1xq1.onrender.com/api/auth/signup",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            fullName,
+            username,
+            password,
+            confirmPassword,
+            gender,
+          }),
+        }
+      );
 
       const data = await res.json();
       if (data.error) {
